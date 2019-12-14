8e50fb9c5947654b17fb0f3af8dcf31234052efb
zhangxiaojun <zhangxiaojun@aihuaiedu.cn>
2019-12-14 17:45:49 +0800

    增加小程序api的回调插件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App'
-
+import MinPromise from 'minapp-promise'
+import Store from './store/'
+Vue.prototype.$store = Store
+Vue.prototype.$MinPromise = MinPromise
 Vue.config.productionTip = false
 App.mpType = 'app'
 
